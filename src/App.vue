c380f592d015a70fe2e45083a3b4e72cf91d628b
Sotelo <cgonzalezsotelo@gmail.com>
2021-10-17 19:13:07 -0300

    correcciones desafio

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,20 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">Rick And Morty</b-navbar-brand>
-
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item href="#">Inicio</b-nav-item>
-          <b-nav-item href="#">Buscar</b-nav-item>
-          <b-nav-item href="#">acerca de</b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
+    <Navbar />
     <main>
-      <b-container>
-        <b-row>
-          <b-col cols="4" v-for="(personaje, index) in personajes" :key="index">
-            <b-card
-              :title="personaje.nombre"
-              :img-src="personaje.imagen"
-              img-alt="Image"
-              img-top
-              tag="article"
-              style="max-width: 20rem"
-              class="mb-2 mt-2"
-            >
-            </b-card>
-          </b-col>
-        </b-row>
-      </b-container>
+      <Card />
     </main>
   </div>
 </template>
 
 <script>
+import Navbar from "./components/Navbar.vue";
+import Card from "./components/Card.vue";
 export default {
   name: "App",
-  data: () => ({
-    personajes: [
-      {
-        id: 1,
-        nombre: "Morty",
-        imagen: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
-      },
-      {
-        id: 2,
-        nombre: "Rick",
-        imagen: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-      },
-      {
-        id: 3,
-        nombre: "Summer",
-        imagen: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
-      },
-      {
-        id: 4,
-        nombre: "Beth",
-        imagen: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
-      },
-      {
-        id: 5,
-        nombre: "Jerry",
-        imagen: "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
-      },
-    ],
-  }),
+  components: {
+    Navbar,
+    Card,
+  },
 };
 </script>
-
-<style></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">Rick And Morty</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="#">Inicio</b-nav-item>
+          <b-nav-item href="#">Buscar</b-nav-item>
+          <b-nav-item href="#">acerca de</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
